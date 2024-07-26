97c6b9dd41151c11c57aaafaaa3e3536931d63a3
TruePelmen <oleksii.senyshyn@lnu.edu.ua>
2024-07-26 14:38:44 +0300

    Added in data: - NewsEntity.toDomain() - News.toEntity()

--- app/src/main/java/com/newsofukraine/data/local/LocalRepoImplementation.kt
+++ app/src/main/java/com/newsofukraine/data/local/LocalRepoImplementation.kt
@@ -11,4 +11,12 @@ class LocalRepoImplementation(private val newsDao: NewsDao): LocalRepo {
     override suspend fun getDeleteNews() {
         TODO("Not yet implemented")
     }
+
+    private fun NewsEntity.toDomain(): News {
+        return News(title, img, author, description)
+    }
+
+    private fun News.toEntity(): NewsEntity {
+        return NewsEntity(title, img, author, description)
+    }
 }
